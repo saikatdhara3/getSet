eec75bc57572ad1a063056a8971988632ee35787
Inno <Inno@DESKTOP-TSQ4BJE>
2021-10-21 19:38:32 +0530

    Add project files.

---
+++ getSet/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace getSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\Team\Saikat\getSet\Text.txt";
+
+            List<string> texts = File.ReadAllLines(filePath).ToList();
+
+            while (true)
+            {
+                Console.WriteLine("press 1 for add or 2 for show or other for exit");
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == 1)
+                {
+                    Console.WriteLine("Please write something:");
+                    string add = Console.ReadLine();
+                    texts.Add(add);
+                }
+                else if(input==2)
+                {
+                    foreach (string s in texts)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
+
+ 
